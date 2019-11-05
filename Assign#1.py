08cb5420c1c1e98ad975ef9e5cc922d75ba79376
MushiKhan <57396493+MushiKhan@users.noreply.github.com>
2019-11-05 05:51:49 -0800

    Add files via upload

---
+++ Assign#1.py
@@ -0,0 +1,65 @@
+w="twinkle,"
+
+x="little star,"
+
+z="how i wonder what you are!"
+
+a="up above the world so high,"
+
+b="like a diamond in the sky."
+
+print(w+" " +w+" "+x+"\n "
+           "   "+z+"\n"
+     "        "+a+"\n""        "+b+"\n"
+     +w+" " +w+" "+x+"\n
+ "
+           "   "+z+"\n")
+
+
+# 
+
+
+import platform
+ 
+print(platform.python_version())
+
+
+
+
+
+
+import datetime
+print(datetime.datetime.now())
+
+
+r=input("enter the radius")
+
+pi=3.14
+
+area=2*float(r)*pi
+print(area)
+
+
+
+
+
+num1=int(input("enter the first number"))
+
+num2=int(input("enter the seconde number") )
+
+add=num1+num2
+
+print(add)
+
+
+
+
+
+fname=input("enter your first name ")
+
+lname=input("enter you last name ")
+
+print(lname+" "+fname)
+
+
+
