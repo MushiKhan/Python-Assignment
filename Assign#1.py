031d38638af3926ac5d280c5be5e82a7d7bbbace
Mushtaq Ahmed <mushtaqa1999@gmail.com>
2019-11-05 17:45:27 -0800

    Python Programming

---
+++ Assign#1.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[35]:
+
+
+
+w="twinkle,"
+
+x="little star,"
+
+z="how i wonder what you are!"
+
+a="up above the world so high,"
+
+b="like a diamond in the sky."
+
+print(w+" " +w+" "+x+"\n "
+           "   "+z+"\n"
+     "        "+a+"\n""        "+b+"\n"
+     +w+" " +w+" "+x+"\n
+ "
+           "   "+z+"\n")
+
+
+# 
+In[36]:
+
+
+
+import platform
+ 
+print(platform.python_version())
+
+
+# In[37]:
+
+
+
+import datetime
+print(datetime.datetime.now())
+
+
+
+# In[41]:
+
+
+
+r=input("enter the radius")
+
+pi=3.14
+
+area=2*float(r)*pi
+print(area)
+
+
+# In[43]:
+
+
+
+num1=int(input("enter the first number"))
+
+num2=int(input("enter the seconde number") )
+
+add=num1+num2
+
+print(add)
+
+
+# In[46]:
+
+
+
+fname=input("enter your first name ")
+
+lname=input("enter you last name ")
+
+print(lname+" "+fname)
+
+
+# In[ ]:
+
+
+
+
